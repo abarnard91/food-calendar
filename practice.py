a178d4576f6770c45a5831eda9d64526c41782dc
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2021-11-16 11:18:44 -0600

    Update practice.py
    changed dictionary to dictionaries and lists of dinner foods, updated the dinner picker algorithm to have italian food

--- practice.py
+++ practice.py
@@ -7,13 +7,36 @@ import random
 #print(dinnerList)
 #print(random.choice(dinnerList))
 #print(random.choice(list(dinnerDict.keys())))
-dinnerDict={0:"mexican", 1:"grill", 2:"baked"}
-mexicanList=["tacos", "enchiladas"]
-grillList=["burger","brats"]
+dinnerDict={0:"mexican", 1:"grill", 2:"baked", 3:"italian"}
+mexicanList=['Chicken Tacos',
+             'Beef Tacos',
+             'Ground Turkey Tacos',
+             'Enchiladas',
+             'Mexican Stuffed Peppers',
+             'Salsa Chicken',
+             'Chicken Quesadillas',
+             'Loaded Nachos']
+grillList=['Cheeseburgers',
+           'Impossible Burgers',
+           'Black Bean Burgers',
+           'Grilled Chicken Patties',
+           'Chicken Wings', 
+           'Chicken Thighs',
+           'Chicken Breasts',
+           'Chicken DrumSticks',
+           'PorkChops',
+           'Steaks',
+           'Bratwursts',
+           'Chicken Sausages']
 bakedList=["fish","chicken"]
+italianList=['Lasagna',
+             'Raviolis and Zoodles',
+             'Baked Mastaccoli',
+             'Chicken parmesan',
+             'Italian Sausage and pasta']
 dinnerDictlist=list(dinnerDict.keys())
 print(dinnerDictlist)
-dinnerTypePicker=random.randint(0,2)
+dinnerTypePicker=random.randint(0,3)
 if dinnerTypePicker == dinnerDictlist[0]:
     print("we're having mexican")
     dinner=random.choice(mexicanList)
@@ -25,4 +48,8 @@ if dinnerTypePicker == dinnerDictlist[1]:
 if dinnerTypePicker == dinnerDictlist[2]:
     print("we're having baked food")
     dinner=random.choice(bakedList)
+    print(dinner)
+if dinnerTypePicker == dinnerDictlist[3]:
+    print("we're having italian food")
+    dinner=random.choice(italianList)
     print(dinner)
