cbe059b755f1e730b35bca23a7eed7a8a5f577c0
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2021-11-17 09:58:00 -0600

    Update monthly dinner calendar.py
    Added dinner picker method which picks a random dinner option and lists the genre of meal for the day and added the method to calendar list of the days of the month
    moved month to days calendar to the bottom

--- monthly dinner calendar.py
+++ monthly dinner calendar.py
@@ -1,4 +1,7 @@
-import random, datetime, calendar, keyboard
+import random
+import datetime
+import calendar
+#keyboard
 
 ## Setting up date and time to the present
 thePresent= datetime.datetime.now()
@@ -11,26 +14,7 @@ theCalendar = calendar.Calendar()
 
 monthDays2CalendarList=theCalendar.monthdays2calendar(presentYear, presentMonth)
 
-#iterates the list of weeks in the month
-for Week in monthDays2CalendarList:
-    #iterates list of tuples of (date of month, day of the week)
-    for days in Week:
-        #if day of the week divided by 7 has a remainder of 0 == Monday and so on)
-        #days[0]= day of the month (1-31)
-        if days[1]%7==0:
-            print(str(days[0])+" its Monday!")
-        if days[1]%7==1:
-            print(str(days[0])+" its Tuesday!")
-        if days[1]%7==2:
-            print(str(days[0])+" its Wednesday!")
-        if days[1]%7==3:
-            print(str(days[0])+" its Thursday!")
-        if days[1]%7==4:
-            print(str(days[0])+" its Friday!")
-        if days[1]%7==5:
-            print(str(days[0])+" its Saturday!")
-        if days[1]%7==6:
-            print(str(days[0])+" its Sunday!")
+
 
 
 #dinner dictionary 0=mexican 1=grilled 2=baked dinners 3=italian 4=breakfast 5=slow cooker
@@ -62,8 +46,8 @@ italianList=['Lasagna',
              'Baked Mastaccoli',
              'Chicken parmesan',
              'Italian Sausage and pasta',
-             'Stuffed shells,
-             'Italian stuffed Peppers"]
+             'Stuffed shells',
+             'Italian stuffed Peppers']
 breakfastList=[ "quiche loreine",
                 "breakfast casserole",
                 "french toast",
@@ -76,7 +60,57 @@ crockpotlist=[  "Crockpot chili",
                 "Chicken, Carrots and potatos",
                 "Beef roast",
                 "Pork Roast"]
+def dinnerPicker():
+    dinnerTypePicker=random.randint(0,5)
+    if dinnerTypePicker == dinnerDict[0]:
+        print("we're having mexican")
+        dinner=random.choice(mexicanList)
+        print(dinner)
+    if dinnerTypePicker == dinnerDict[1]:
+        print("we're having grilled food")
+        dinner=random.choice(grillList)
+        print(dinner)
+    if dinnerTypePicker == dinnerDict[2]:
+        print("we're having baked food")
+        dinner=random.choice(bakedList)
+        print(dinner)
+    if dinnerTypePicker == dinnerDict[3]:
+        print("we're having italian food")
+        dinner=random.choice(italianList)
+        print(dinner)
+    if dinnerTypePicker == dinnerDict[4]:
+        print("we're having Breakfast food")
+        dinner=random.choice(breakfastList)
+        print(dinner)
+    if dinnerTypePicker == dinnerDict[5]:
+        print("we're having slow cooker food")
+        dinner=random.choice(crockpotList)
+        print(dinner)
 
-
-
-               
+#iterates the list of weeks in the month
+for Week in monthDays2CalendarList:
+    #iterates list of tuples of (date of month, day of the week)
+    for days in Week:
+        #if day of the week divided by 7 has a remainder of 0 == Monday and so on)
+        #days[0]= day of the month (1-31)
+        if days[1]%7==0:
+            print(str(days[0])+" its Monday!")
+            dinnerPicker()
+        if days[1]%7==1:
+            print(str(days[0])+" its Tuesday!")
+            dinnerPicker()
+        if days[1]%7==2:
+            print(str(days[0])+" its Wednesday!")
+            dinnerPicker()
+        if days[1]%7==3:
+            print(str(days[0])+" its Thursday!")
+            dinnerPicker()
+        if days[1]%7==4:
+            print(str(days[0])+" its Friday!")
+            dinnerPicker()
+        if days[1]%7==5:
+            print(str(days[0])+" its Saturday!")
+            dinnerPicker()
+        if days[1]%7==6:
+            print(str(days[0])+" its Sunday!")
+            dinnerPicker()
